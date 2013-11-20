bd0ea3e5d24d8ddd2c469801cfade47c8cd06e64
Steven Holdsworth <sholdsworth@gmail.com>
2013-11-20 00:30:45 +0000

    Completed Assignment 2 (not great bit messy)

--- assignments/simulations/src/main/scala/simulations/EpidemySimulator.scala
+++ assignments/simulations/src/main/scala/simulations/EpidemySimulator.scala
@@ -2,7 +2,28 @@ package simulations
 
 import math.random
 
-class EpidemySimulator extends Simulator {
+trait DiseaseRules {
+    val prevalence = 0.01
+    val transmissibility = 0.4
+    val chanceOfMortality = 0.25
+}
+
+trait InfectionPeriods {
+    val becomesContagiousPeriod = 6
+    val becomesCriticalPeriod = 8
+    val immuneDelay = 2
+    val healthyDelay = 2
+}
+
+trait AdditionalParameters { 
+    val airplaneMode = false
+    val airplaneChance = 0.01
+    val mobilityMode = true
+    val chosenFewMode = false
+    val vipChance = 0.05
+}
+
+class EpidemySimulator extends Simulator with DiseaseRules with InfectionPeriods with AdditionalParameters {
 
   def randomBelow(i: Int) = (random * i).toInt
 
@@ -10,43 +31,25 @@ class EpidemySimulator extends Simulator {
     val population: Int = 300
     val roomRows: Int = 8
     val roomColumns: Int = 8
-
-    // Base rules
-    val prevalenceRate = 0.01
-    val transmissibilityRate = 0.4
-    val incubationDelay = 6
-    val deathDelay = 8
-    val deathChance = 0.25
-    val immuneDelay = 2
-    val healthyDelay = 2
-
-    // Add-ins
-    val airplaneMode = false
-    val airplaneChance = 0.01
-
-    val mobilityMode = true
-
-    val chosenFewMode = false
-    val vipChance = 0.05
   }
 
   import SimConfig._
 
-  val persons: List[Person] = for (id <- (1 to population).toList) yield new Person(id)
-
-  def personsByField = persons.groupBy(person => (person.row, person.col))
+  val persons: List[Person] = 
+    for (id <- (1 to population).toList) 
+      yield new Person(id)
 
   def neighbours(row: Int, col: Int) = {
-    val directions = List((1, 0), (-1, 0), (0, 1), (0, -1))
+    val targets = List((1, 0), (-1, 0), (0, 1), (0, -1))
     for {
-      direction <- directions
-      nRow = (((row + direction._1) % roomRows) + roomRows) % roomRows //to avoid negatives
-      nCol = (((col + direction._2) % roomColumns) + roomColumns) % roomColumns
+      target <- targets
+      nRow = (((row + target._1) % roomRows) + roomRows) % roomRows //to avoid negatives
+      nCol = (((col + target._2) % roomColumns) + roomColumns) % roomColumns
     } yield (nRow, nCol)
   }
 
   class Person(val id: Int) {
-    val initialInfected = (population * prevalenceRate).toInt
+    val initialInfected = (population * prevalence).toInt
     var infected = id <= initialInfected
     var sick = false
     var immune = id <= (population * vipChance).toInt + initialInfected && id > initialInfected
@@ -56,73 +59,77 @@ class EpidemySimulator extends Simulator {
     var row: Int = randomBelow(roomRows)
     var col: Int = randomBelow(roomColumns)
 
-    def moveAction {
-
-      def infect() {
-        val personsInRoom = personsByField.get((row, col))
-        if (personsInRoom.isDefined && personsInRoom.get.exists(p => p.infected) && random <= transmissibilityRate) {
+    def move {
+      if (dead)
+        return
+      
+      def tryInfect() {
+        val personsInRoom = persons.groupBy(person => (person.row, person.col)).get((row, col))
+        if (personsInRoom.isDefined && personsInRoom.get.exists(p => p.infected) && random <= transmissibility) {
           infected = true
-          afterDelay(incubationDelay)(sicken)
+          afterDelay(becomesContagiousPeriod)(setSick)
         }
       }
-
-      if (!dead) {
-        if (airplaneMode && random <= airplaneChance) {
-          row = randomBelow(roomRows)
-          col = randomBelow(roomColumns)
-          infect()
-        } else {
-
-          val healthyNeighbours = neighbours(row, col).filter(field => {
-            val persons = personsByField.get(field)
-            !persons.isDefined || !persons.get.exists(p => p.sick || p.dead)
-          })
-
-          if (!healthyNeighbours.isEmpty) {
-            val neighbour = healthyNeighbours(randomBelow(healthyNeighbours.length))
-            row = neighbour._1
-            col = neighbour._2
-            infect()
-          }
-
+      
+      if (airplaneMode && random <= airplaneChance) {
+        row = randomBelow(roomRows)
+        col = randomBelow(roomColumns)
+        tryInfect()
+      } else {
+        val allNeighbours = neighbours(row, col);
+        val neighboursNotInfected = allNeighbours.filter(field => {
+          val people = persons.groupBy(person => (person.row, person.col)).get(field)
+          !people.isDefined || !people.get.exists(p => p.sick || p.dead)
+        });
+        if (!neighboursNotInfected.isEmpty) {
+          val adjacentNeighbour = neighboursNotInfected(randomBelow(neighboursNotInfected.length))
+          row = adjacentNeighbour._1; col = adjacentNeighbour._2
+          tryInfect()
         }
-
-        val delay = randomBelow(5) + 1
-        afterDelay(if (mobilityMode) (if (sick) delay * 4 else delay * 2) else delay)(moveAction)
       }
-
+      
+      val delay = randomBelow(5) + 1
+      afterDelay(if (mobilityMode) (if (sick) delay * 4 else delay * 2) else delay)(move)
     }
 
-    def sicken() {
+    def setSick() {
       sick = true
-      afterDelay(deathDelay)(kill)
+      afterDelay(becomesCriticalPeriod)(tryKill)
     }
 
-    def kill() {
-      if (random <= deathChance) dead = true
-      else afterDelay(immuneDelay)(immunize)
-    }
-
-    def immunize() {
-      if (!dead) {
-        sick = false
-        immune = true
+    def tryKill() {
+      
+      def developImmunity() {
+        if (dead)
+    	  return
+    	  
+        def recover() {
+          if (dead)
+            return
+        
+          infected = false
+          immune = false
+        }
+    
+        sick = false; immune = true
         afterDelay(healthyDelay)(recover)
       }
-    }
-
-    def recover() {
-      if (!dead) {
-        infected = false
-        immune = false
-      }
+      
+      if (random <= chanceOfMortality) 
+        dead = true
+      else 
+        afterDelay(immuneDelay)(developImmunity)
     }
 
     if (infected) {
-      afterDelay(incubationDelay)(sicken)
+      afterDelay(becomesContagiousPeriod)(setSick)
     }
 
     val delay = randomBelow(5) + 1
-    afterDelay(if (mobilityMode) (if (sick) delay * 4 else delay * 2) else delay)(moveAction)
+    afterDelay(
+        if (mobilityMode) 
+    		(if (sick) delay * 4 else delay * 2) 
+        else delay
+    )(move)
   }
 }
