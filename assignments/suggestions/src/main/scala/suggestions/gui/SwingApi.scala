ddc555b31f2a7bc4f007a9429cf5827c9c440ea0
Steven Holdsworth <sholdsworth@gmail.com>
2013-12-10 23:12:43 +0000

    Not using Reactions directly WingApi.scala, removes focus exceptions

--- assignments/suggestions/src/main/scala/suggestions/gui/SwingApi.scala
+++ assignments/suggestions/src/main/scala/suggestions/gui/SwingApi.scala
@@ -9,7 +9,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{ Try, Success, Failure }
 import scala.swing.Reactions.Reaction
 import scala.swing.event.Event
-import rx.lang.scala.{Subscription, Observer, Observable}
+import rx.lang.scala.Observable
 import rx.lang.scala.subscriptions.Subscription
 
 /** Basic facilities for dealing with Swing-like components.
@@ -47,30 +47,43 @@ trait SwingApi {
 
   implicit class TextFieldOps(field: TextField) {
 
-    /** Returns a stream of text field values entered in the given text field.
-      *
-      * @param field the text field
-      * @return an observable with a stream of text field updates
-      */
-    def textValues: Observable[String] = Observable { observer =>
-      val rx: Reaction = { case ValueChanged(x) => observer.onNext(x.text) }
-      field.subscribe(rx)
-      Subscription { field.unsubscribe(rx) }
-    }
+    /**
+     * Returns a stream of text field values entered in the given text field.
+     *
+     * @param field the text field
+     * @return an observable with a stream of text field updates
+     */
+    def textValues: Observable[String] = Observable(observer => {
+      field subscribe {
+        case ValueChanged(tf) => observer.onNext (tf.text)
+        case _ =>
+      }
+      Subscription(field.unsubscribe {
+        case _ => observer.onCompleted
+      })
+    })
+
+
   }
 
   implicit class ButtonOps(button: Button) {
 
-    /** Returns a stream of button clicks.
+    /**
+     * Returns a stream of button clicks.
      *
      * @param field the button
      * @return an observable with a stream of buttons that have been clicked
      */
-    def clicks: Observable[Button] = Observable { observer =>
-      val rx: Reaction = { case ButtonClicked(x) => observer.onNext(x) }
-      button.subscribe(rx)
-      Subscription { button.unsubscribe(rx) }
-    }
+    def clicks: Observable[Button] = Observable(observer => {
+      button subscribe {
+        case ButtonClicked(b) => observer.onNext (b)
+        case _ =>
+      }
+      Subscription(button.unsubscribe {
+        case _ => observer.onCompleted
+      })
+    })
+
   }
 
 }
