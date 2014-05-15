3851bb388749d5d7477e42c10d95aa1353879dd8
Steven Holdsworth <sholdsworth@gmail.com>
2014-05-15 18:43:42 +0100

    refined wk 1 solution

--- FunProg2014/assignments/recfun/src/main/scala/recfun/Main.scala
+++ FunProg2014/assignments/recfun/src/main/scala/recfun/Main.scala
@@ -16,8 +16,8 @@ object Main {
    */
   def pascal(c: Int, r: Int): Int = (c,r) match {
      case (0, _) => 1
-     case (c, r)  if (c==r) => 1
-     case (c,r) => pascal(c,r-1)+pascal(c-1,r-1)
+     case (c, r) if (c==r) => 1
+     case (c, r) => pascal(c,r-1)+pascal(c-1,r-1)
   }
 
   /**
@@ -25,11 +25,9 @@ object Main {
    */
   def balance(chars: List[Char]): Boolean = {
     def bal(chars: List[Char], count: Int) : Boolean = {
-      if (chars.isEmpty && count == 0) true
-      else if (chars.isEmpty && count != 0) false
-      else if (count < 0) false
+      if (chars.isEmpty || count < 0) count == 0
       else if (chars.head == ')') bal(chars.tail, (count-1))
-      else if (chars.head == '(') bal(chars.tail, (count+1))
+      else if (chars.head == '(') bal(chars.tail, (count+1)) 
       else bal(chars.tail, count)
     }
     bal(chars, 0)
@@ -40,9 +38,9 @@ object Main {
    * Exercise 3
    */
   def countChange(money: Int, coins: List[Int]): Int = (money, coins) match {
-     case (0, _) => 1
-     case (m, _) if m < 0 => 0
-     case (_, cs) if cs.isEmpty => 0
-     case (m, cs) => countChange(m - cs.head, cs) + countChange(m, cs.tail) 
+    case (0, _) => 1
+    case (m, _) if m < 0 => 0
+    case (_, cs) if cs.isEmpty => 0
+    case (m, cs) => countChange(m - cs.head, cs) + countChange(m, cs.tail) 
   }
 }
